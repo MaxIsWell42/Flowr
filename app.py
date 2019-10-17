0f90a5171ef9872678e732b7df7565e472c0a647
HexSeal <maxfin13@gmail.com>
2019-10-16 22:47:50 -0700

    [add] Comments for everything and what needs to get done. Trying to figure out the Google Places API, and complete the flowr form

--- app.py
+++ app.py
@@ -5,13 +5,17 @@ from datetime import datetime
 import time
 import os
 
+#TODO Finish questions+answers, use google place api to get local restaurants and use answers to query the final page
+
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Flowr')
 # db instantiation
 client = MongoClient(host=host)
 
 db = client.get_default_database()
-# collection name
+# collection names
 flowrs = db.flowr
+questions = db.question
+answers = db.answer
 # flask app name
 app = Flask(__name__)
 
@@ -25,15 +29,14 @@ def flowr_homepage():
     #     # if there is a post request on the index page
     #     pass
 
-
 @app.route('/flowr/new')
 def flowr_new():
     """ Where the user creates a flowr chart. """
     return render_template('flowr_new.html', flowr={}, title='New Flowr')
 
-@app.route('/flowr')
+@app.route('/flowr' methods=['POST'])
 def flowr_show():
-    """ Where the user can submit and see their fully created flowr """
+    """ Where the user can submit their fully created flowr """
     flowr = {
         'title': request.form.get('title'),
         'questions': request.form.get('questions'),
@@ -42,6 +45,18 @@ def flowr_show():
     }
     flowr_id = flowrs.insert(flowr).inserted_id
     return render_template('flowr_show.html', flowr_id=flowr_id)
+
+@app.route('flowr/<flowr_choice>', methods=)
+def flowr_choice():
+    #TODO: implement differenct choices sending you to differnet pages
+    if flowr_choice == choice1:
+        return render_template('/flowr')
+    elif flowr_choice == choice2:
+        return render_template('/flowr')
+
+@app.route('flowr/<flowr_id>')
+def flowr_final():
+""" final page of flowr, where the user can see the final suggestion. Future to include other suggestions and ability to create events """
 
 
 if __name__ == "__main__":
