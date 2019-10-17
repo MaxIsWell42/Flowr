d150b34dbac583e1842ed5caef72de9b8f588aa3
HexSeal <maxfin13@gmail.com>
2019-10-17 10:30:43 -0700

    [add] googlemaps api not importing for some reason but still works? All paths setup, just need questions and answers

--- app.py
+++ app.py
@@ -2,43 +2,149 @@ from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 from datetime import datetime
-import googlemaps
 import time
 import os
+import googlemaps
+import requests
 
-#TODO Finish questions+answers, use google place api to get local restaurants and use answers to query the final page
+#TODO Finish questions+answers, use answers to query the final page, finish wireframes.
 
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Flowr')
 
 # db instantiation
 client = MongoClient(host=host)
 
-# Defining the client
-gmaps = googlempaps.Client(API_KEY)
-
 # Defaulting to a database
 db = client.get_default_database()
 
 # collection names
 flowrs = db.flowr
-questions = db.question
+questions = db.questions
 answers = db.answer
+
+# Defining the client
+key = os.environ.get('API_KEY')
+gmaps = googlemaps.Client(key=key)
 
 # flask app name
 app = Flask(__name__)
 
 # Places API key
-API_KEY = AIzaSyC_3HdF4PGrktqWsyzNtGHlv0DDjNqk7Q0
+API_KEY = os.environ.get('API_KEY')
 
 @app.route('/')
-@app.route('/index', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET'])
 def flowr_homepage():
     
     if request.method == 'GET':
-        return render_template('flowr_hp.html')
-    # if request.method == 'POST':
-    #     # if there is a post request on the index page
-    #     pass
+    
+        #print(details)
+        return render_template('index.html')
+
+@app.route('/thai',methods=['GET'])
+def thai():
+    if request.method == 'GET':
+        query = 'Thai resturant'
+        results = gmaps.places(query, radius=2, location=['37.773972', '-122.431297'], language='English', max_price=4)
+        photo_id = results['results'][0]['photos'][0]['photo_reference']
+        print(photo_id)
+        #gmaps.places_photo(photo_id)
+        f = open('resturant_photo.jpg', 'wb')
+        for chunk in gmaps.places_photo(photo_id, max_width=100):
+            if chunk:
+                f.write(chunk)
+        f.close()
+        return results
+
+@app.route('/mexican', methods=['GET'])
+def mexican():
+    if request.method == 'GET':
+        query = 'Mexican resturant'
+        results = gmaps.places(query, radius=2, location=['37.773972', '-122.431297'], language='English', max_price=4)
+        photo_id = results['results'][0]['photos'][0]['photo_reference']
+        print(photo_id)
+        #gmaps.places_photo(photo_id)
+        f = open('resturant_photo.jpg', 'wb')
+        for chunk in gmaps.places_photo(photo_id, max_width=100):
+            if chunk:
+                f.write(chunk)
+        f.close()
+        return results
+
+@app.route('/chinese', methods=['GET'])
+def chinese():
+    if request.method == 'GET':
+        query = 'Chinese resturant'
+        results = gmaps.places(query, radius=2, location=['37.773972', '-122.431297'], language='English', max_price=4)
+        photo_id = results['results'][0]['photos'][0]['photo_reference']
+        print(photo_id)
+        #gmaps.places_photo(photo_id)
+        f = open('resturant_photo.jpg', 'wb')
+        for chunk in gmaps.places_photo(photo_id, max_width=100):
+            if chunk:
+                f.write(chunk)
+        f.close()
+        return results
+
+@app.route('/american', methods=['GET'])
+def american():
+    if request.method == 'GET':
+        query = 'American resturant'
+        results = gmaps.places(query, radius=2, location=['37.773972', '-122.431297'], language='English', max_price=4)
+        photo_id = results['results'][0]['photos'][0]['photo_reference']
+        print(photo_id)
+        #gmaps.places_photo(photo_id)
+        f = open('resturant_photo.jpg', 'wb')
+        for chunk in gmaps.places_photo(photo_id, max_width=100):
+            if chunk:
+                f.write(chunk)
+        f.close()
+        return results
+
+@app.route('/indian', methods=['GET'])
+def indian():
+    if request.method == 'GET':
+        query = 'Indian resturant'
+        results = gmaps.places(query, radius=2, location=['37.773972', '-122.431297'], language='English', max_price=4)
+        photo_id = results['results'][0]['photos'][0]['photo_reference']
+        print(photo_id)
+        #gmaps.places_photo(photo_id)
+        f = open('resturant_photo.jpg', 'wb')
+        for chunk in gmaps.places_photo(photo_id, max_width=100):
+            if chunk:
+                f.write(chunk)
+        f.close()
+        return results
+
+@app.route('/japanese', methods=['GET'])
+def japanese():
+    if request.method == 'GET':
+        query = 'Japanese resturant'
+        results = gmaps.places(query, radius=2, location=['37.773972', '-122.431297'], language='English', max_price=4)
+        photo_id = results['results'][0]['photos'][0]['photo_reference']
+        print(photo_id)
+        #gmaps.places_photo(photo_id)
+        f = open('resturant_photo.jpg', 'wb')
+        for chunk in gmaps.places_photo(photo_id, max_width=100):
+            if chunk:
+                f.write(chunk)
+        f.close()
+        return results
+
+@app.route('/italian', methods=['GET'])
+def italian():
+    if request.method == 'GET':
+        query = 'Italian resturant'
+        results = gmaps.places(query, radius=2, location=['37.773972', '-122.431297'], language='English', max_price=4)
+        photo_id = results['results'][0]['photos'][0]['photo_reference']
+        print(photo_id)
+        #gmaps.places_photo(photo_id)
+        f = open('resturant_photo.jpg', 'wb')
+        for chunk in gmaps.places_photo(photo_id, max_width=100):
+            if chunk:
+                f.write(chunk)
+        f.close()
+        return results
 
 @app.route('/flowr/new')
 def flowr_new():
@@ -57,7 +163,7 @@ def flowr_show():
     flowr_id = flowrs.insert(flowr).inserted_id
     return render_template('flowr_show.html', flowr_id=flowr_id)
 
-@app.route('flowr/<flowr_choice>', methods=['GET'])
+@app.route('/flowr/<flowr_choice>', methods=['GET'])
 def flowr_choice():
     #TODO: implement differenct choices sending you to differnet pages
     if flowr_choice == choice1:
@@ -65,9 +171,8 @@ def flowr_choice():
     elif flowr_choice == choice2:
         return render_template('/flowr')
 
-@app.route('flowr/<flowr_id>')
+@app.route('/flowr/<flowr_id>')
 def flowr_final():
-""" final page of flowr, where the user can see the final suggestion. Future to include other suggestions and ability to create events """
     return render_template('/flowr/<flowr_id>')
 
 if __name__ == "__main__":
